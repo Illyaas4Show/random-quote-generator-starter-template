08b0ecae809cce8fde37c1cc4c81ccb9ac26a429
Illyaas4Show <96283624+Illyaas4Show@users.noreply.github.com>
2022-10-20 17:53:59 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "../main.css";
+
+import { colours } from "./colours.js";
+import { quotes } from "./quotes.js";
+
+const cardElement = document.querySelector(".card");
+const quoteElement = document.querySelector(".quote");
+const imageElement = document.querySelector(".image");
+const buttonElement = document.querySelector(".button");
+
+// if you change the number of images, update the value for this variable below
+const numOfPics = 10;
